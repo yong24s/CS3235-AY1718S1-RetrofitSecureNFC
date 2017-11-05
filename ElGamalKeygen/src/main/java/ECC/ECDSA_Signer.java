424acd858b5e9d31ed9705fc171e83ba4d81b0a7
Yong Hao TENG <yonghao.teng@gmail.com>
2017-11-06 01:19:35 +0800

    Add support to read x509 certificates

--- ElGamalKeygen/src/main/java/ECC/ECDSA_Signer.java
+++ ElGamalKeygen/src/main/java/ECC/ECDSA_Signer.java
@@ -1,5 +1,7 @@
 package ECC;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
@@ -10,6 +12,8 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Security;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.security.interfaces.ECPublicKey;
 import java.security.spec.ECPoint;
 import java.security.spec.ECPublicKeySpec;
@@ -60,17 +64,48 @@ public class ECDSA_Signer {
 //        ECPublicKey pk = (ECPublicKey) kf.generatePublic(pubKeySpec);
 //        
         Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-        Reader rdr = new StringReader("-----BEGIN PUBLIC KEY-----\n"
-                +"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEww2MbZ2l0AziUHfbRmjyiXd/rdmG\n"
-                +"l3E22HX644ipw7+7GNlkRas8Eu7iEbCipSVE492TbvBtlH9equJkHKMcjw==\n"
-                +"-----END PUBLIC KEY-----\n"); // or from file etc.
+//        Reader rdr = new StringReader("-----BEGIN PUBLIC KEY-----\n"
+//                +"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEww2MbZ2l0AziUHfbRmjyiXd/rdmG\n"
+//                +"l3E22HX644ipw7+7GNlkRas8Eu7iEbCipSVE492TbvBtlH9equJkHKMcjw==\n"
+//                +"-----END PUBLIC KEY-----\n"); // or from file etc.
 
-       
+        Reader rdr = new StringReader("-----BEGIN CERTIFICATE-----\n" + 
+"MIIB2zCCAYGgAwIBAgIJAJ9UWB+CT5q7MAoGCCqGSM49BAMCMEoxCzAJBgNVBAYT\n"+
+"AlNHMQswCQYDVQQIDAJTRzELMAkGA1UEBwwCU0cxITAfBgNVBAoMGEludGVybmV0\n"+
+"IFdpZGdpdHMgUHR5IEx0ZDAeFw0xNzExMDUxNzA4NDlaFw0xODExMDUxNzA4NDla\n"+
+"MEoxCzAJBgNVBAYTAlNHMQswCQYDVQQIDAJTRzELMAkGA1UEBwwCU0cxITAfBgNV\n"+
+"BAoMGEludGVybmV0IFdpZGdpdHMgUHR5IEx0ZDBZMBMGByqGSM49AgEGCCqGSM49\n"+
+"AwEHA0IABMMNjG2dpdAM4lB320Zo8ol3f63ZhpdxNth1+uOIqcO/uxjZZEWrPBLu\n"+
+"4hGwoqUlROPdk27wbZR/XqriZByjHI+jUDBOMB0GA1UdDgQWBBT6J4jHCuOlkvxd\n"+
+"jSIhQa+638yX0zAfBgNVHSMEGDAWgBT6J4jHCuOlkvxdjSIhQa+638yX0zAMBgNV\n"+
+"HRMEBTADAQH/MAoGCCqGSM49BAMCA0gAMEUCIC01ZqikAUV0im38RqW3JE+k6SPq\n"+
+"o+rAiXXWXoGCzRL+AiEA4L3NC0muqSh+MZRXy31AAjh/hmiybjbsG6ZoJJGNSis=\n"+
+"-----END CERTIFICATE-----"); // or from file etc.
+        
+        String str = ("-----BEGIN CERTIFICATE-----\n" + 
+                "MIIB2zCCAYGgAwIBAgIJAJ9UWB+CT5q7MAoGCCqGSM49BAMCMEoxCzAJBgNVBAYT\n"+
+                "AlNHMQswCQYDVQQIDAJTRzELMAkGA1UEBwwCU0cxITAfBgNVBAoMGEludGVybmV0\n"+
+                "IFdpZGdpdHMgUHR5IEx0ZDAeFw0xNzExMDUxNzA4NDlaFw0xODExMDUxNzA4NDla\n"+
+                "MEoxCzAJBgNVBAYTAlNHMQswCQYDVQQIDAJTRzELMAkGA1UEBwwCU0cxITAfBgNV\n"+
+                "BAoMGEludGVybmV0IFdpZGdpdHMgUHR5IEx0ZDBZMBMGByqGSM49AgEGCCqGSM49\n"+
+                "AwEHA0IABMMNjG2dpdAM4lB320Zo8ol3f63ZhpdxNth1+uOIqcO/uxjZZEWrPBLu\n"+
+                "4hGwoqUlROPdk27wbZR/XqriZByjHI+jUDBOMB0GA1UdDgQWBBT6J4jHCuOlkvxd\n"+
+                "jSIhQa+638yX0zAfBgNVHSMEGDAWgBT6J4jHCuOlkvxdjSIhQa+638yX0zAMBgNV\n"+
+                "HRMEBTADAQH/MAoGCCqGSM49BAMCA0gAMEUCIC01ZqikAUV0im38RqW3JE+k6SPq\n"+
+                "o+rAiXXWXoGCzRL+AiEA4L3NC0muqSh+MZRXy31AAjh/hmiybjbsG6ZoJJGNSis=\n"+
+                "-----END CERTIFICATE-----"); 
+      
         try {
-            org.bouncycastle.util.io.pem.PemObject spki;
-            spki = new org.bouncycastle.util.io.pem.PemReader(rdr).readPemObject();
-            return KeyFactory.getInstance("EC","BC").generatePublic(new X509EncodedKeySpec(spki.getContent()));
             
+            CertificateFactory fact = CertificateFactory.getInstance("X.509");
+            InputStream is = new ByteArrayInputStream(str.getBytes());
+            X509Certificate cecerr = (X509Certificate) fact.generateCertificate(is);
+            PublicKey key = cecerr.getPublicKey();
+            return key;
+//            org.bouncycastle.util.io.pem.PemObject spki;
+//            spki = new org.bouncycastle.util.io.pem.PemReader(rdr).readPemObject();
+//            return KeyFactory.getInstance("EC","BC").generatePublic(new X509EncodedKeySpec(spki.getContent()));
+//            
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
