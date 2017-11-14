b0fa1c44c1213662ba0a9d582f101f65ebe868bc
Yong Hao TENG <yonghao.teng@gmail.com>
2017-11-14 20:34:45 +0800

    Make app trigger verification code when open Update demo APK

--- AndroidApp/app/src/main/java/com/retrofit/nfc/retrofitsecurenfc/MainActivity.java
+++ AndroidApp/app/src/main/java/com/retrofit/nfc/retrofitsecurenfc/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 //        init();
-//        handleIntent(getIntent());
+        handleIntent(getIntent());
     }
 
     @Override
